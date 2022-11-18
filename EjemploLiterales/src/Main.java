404f68a859d3984afe6b15f42527b124c23f1336
Adrian <afernandeztorres@danielcastelao.org>
2022-11-18 09:41:01 +0100

    programa

---
+++ EjemploLiterales/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("o alumnado\n de dam1\n esta\t ben");
+        //System.out.print(" ******** ");
+        int dato1 = 5;
+        long dato2 = 10L;
+        long dato3 = 10;
+        double dato4 = 3.6;
+        //float dato5 = 5.5;
+        float dato6 = 6.6f;
+        System.out.println("el resultado es = " +dato1);
+        System.out.println("el resultado es = " +dato1+ " y el dato2 vale = "+dato2);
+
+    }
+}
